e5156449267305a03b78aa89e7a326b738995e98
Derrick <derrickgnaba@gmail.com>
2022-05-17 17:06:47 +0000

    mon site perso

---
+++ script.js
@@ -0,0 +1,9 @@
+$('.mocassins').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  let gauche = document.querySelector('.slick-next');
+  gauche.style.color='black';
